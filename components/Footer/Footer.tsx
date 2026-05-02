2594ca8b05a010e709e317f931ae7be7e9bf6b20
LudSkop <luda.skopenko80@gmail.com>
2026-05-02 13:22:06 +0300

    added style

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: Liudmila Skopenko</p>
+          <p>Developer: Liudmyla Skopenko</p>
           <p>
             Contact us:
             <a href="mailto:student@notehub.app">student@notehub.app</a>
